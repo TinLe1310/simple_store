9dbb445f8a5d409958e5855a3ab6abab276ddace
Tin Le <tintrungle1310@gmail.com>
2024-03-04 13:58:24 -0600

    Manipulate data into database with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
 Product.delete_all
 
-Product.create(
-  title: "Text sample",
-  description: "Testing product to valid model",
-  stock_quantity: 20)
+for a in 1..676 do
+  Product.create(
+  title: Faker::Food.ingredient,
+  description: "Product's description",
+  price: Faker::Commerce.price,
+  stock_quantity: Faker::Number.number)
+end
