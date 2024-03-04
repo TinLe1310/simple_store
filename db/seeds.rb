4aead6645b0a7cb45c37fad71c1d089dba5051c1
Tin Le <tintrungle1310@gmail.com>
2024-03-04 13:39:48 -0600

    Set validation for Product model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.delete_all
+
+Product.create(
+  title: "Text sample",
+  description: "Testing product to valid model",
+  stock_quantity: 20)
